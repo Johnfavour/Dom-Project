ef09aadd6daa274b51d404a2ae8f6817a9619045
Johnfavour <favourjohn@gmail.com>
2023-08-06 08:42:56 +0100

    Implemented shopping cart functionality with item manipulation.

---
+++ dom-checkpoint/script.js
@@ -0,0 +1,129 @@
+//Interactive shopping cart with animations.
+const openShopping = document.querySelector(".shopping");
+const closeShopping = document.querySelector(".closeShopping");
+const list = document.querySelector(".list");
+const listCard = document.querySelector(".listCard");
+const total = document.querySelector(".total");
+const body = document.querySelector("body");
+const quantity = document.querySelector(".quantity");
+
+    openShopping.addEventListener("click", () => {
+        body.classList.add("active")
+    })
+    closeShopping.addEventListener("click", () => {
+        body.classList.remove("active")
+    })
+
+
+//Product catalog with image prices
+let products = [
+    {
+        id: 1,
+        name: "PRODUCT 1",
+        images: "1.PNG",
+        price: 2000
+    },
+    {
+        id: 2,
+        name: "PRODUCT 2",
+        images: "2.PNG",
+        price: 1800
+    },
+    {
+        id: 3,
+        name: "PRODUCT 3",
+        images: "3.PNG",
+        price: 1500
+    },
+    {
+        id: 4,
+        name: "PRODUCT 4",
+        images: "4.PNG",
+        price: 2200
+    },
+    {
+        id: 5,
+        name: "PRODUCT 5",
+        images: "5.PNG",
+        price: 2500
+    },
+    {
+        id: 6,
+        name: "PRODUCT 6",
+        images: "6.PNG",
+        price: 3000
+    },
+]
+
+//Dynamic product listing with cart
+let listCards = [];
+
+const initApp = () => {
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("item");
+        newDiv.innerHTML = `
+        <img src="img/${value.images}">
+        <div class="title">${value.name}</div>
+        <div class="price">${value.price.toLocaleString()}</div>
+        <button onclick="addToCard(${key})">Add to Cart</button>
+        `
+        list.appendChild(newDiv)
+    })
+}
+
+initApp()
+
+//Dynamic card manipulation with quantity
+const addToCard = (key) => {
+    if(listCards[key] == null){
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1
+    } 
+    
+    reloadCard();
+}
+const reloadCard = () => {
+    listCard.innerHTML = "";
+    let count = 0;
+    let totalPrice = 0;
+
+    listCards.forEach((value, key) => {
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+
+        if(value != null){
+            let newDiv = document.createElement("li");
+            newDiv.innerHTML = `
+                <div><img src="img/${value.images}"></div>
+                <div class="cardTitle">${value.name}</div>
+                <div class="cardPrice">${value.price.toLocaleString()}</div>
+
+                <div>
+                <button style="background-color: #560bad"
+                class="cardButton" onclick="changeQuantity(${key}, ${value.quantity - 1})"> - </button>
+
+                <div class="count">${value.quantity}</div>
+
+                <button style="background-color: #560bad"
+                class="cardButton" onclick="changeQuantity(${key}, ${value.quantity + 1})"> + </button>
+                </div>
+                `
+
+            listCard.appendChild(newDiv);
+        }
+        total.innerText = totalPrice.toLocaleString();
+        quantity.innerText = count;
+    })
+}
+
+const changeQuantity = (key, quantity) => {
+    if(quantity == 0){
+        delete listCards[key]
+    }
+    else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price
+    }
+    reloadCard()
+}
